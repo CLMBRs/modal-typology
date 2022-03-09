c0e41981251eb68ce8754fbc934bdfac87b78181
Shane Steinert-Threlkeld <ssshanest@gmail.com>
2022-03-09 10:30:32 -0800

    sources in cldf

--- build.sh
+++ build.sh
@@ -4,7 +4,8 @@ cp basic-format/all_observations.csv cldf-format/raw
 cp basic-format/all_metadata.csv cldf-format/raw
 cp basic-format/forces.csv cldf-format/raw
 cp basic-format/flavors.csv cldf-format/raw
-cp basic-format/ref.bib cldf-format/raw
+cp basic-format/sources.bib cldf-format/raw
+cp cldf-format/raw/sources.bib cldf-format/
 
 cd cldf-format
 cldfbench makecldf cldfbench_modals.py
